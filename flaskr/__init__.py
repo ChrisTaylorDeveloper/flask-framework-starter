0b5c408100f91881504e59138a1978de1b57fc05
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-02-24 20:44:41 +0700

    Delete comments.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,10 +24,8 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
     @app.route("/hello")
     def hello():
-        # return 'Hello, World!'
         return render_template("hello.html")
 
     return app
