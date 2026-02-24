0364372adf793c47828a6eb12b55b5e4e8e0d35f
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-02-24 11:21:34 +0700

    Update the /hello route to use its own template.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,6 +28,6 @@ def create_app(test_config=None):
     @app.route("/hello")
     def hello():
         # return 'Hello, World!'
-        return render_template("base.html")
+        return render_template("hello.html")
 
     return app
